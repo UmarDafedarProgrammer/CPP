627fe17f8c71bd5f7369a4c2ca88f81b166c659b
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-12-26 18:08:04 +0530

    Update 1.6.1.CharacterDataType.cpp

--- 1.6.1.CharacterDataType.cpp
+++ 1.6.1.CharacterDataType.cpp
@@ -1,5 +1,8 @@
 /******************************************************************************
 Character Data Type
+
+signe char -> char -> 1 byte -> MSB for identifying the sign
+unsigne char -> 1 byte
 *******************************************************************************/
 #include <iostream>
 
@@ -20,8 +23,17 @@ int main()
     cout<<"Range : "<<int(numeric_limits<char>::min())<<" to "<<int(numeric_limits<char>::max())<<endl;
     
     // float to char
-    c = 94.37; cout<<"Assign 94.37 to character$ "<<c<<", ASCII-"<<int(c)<<endl;
-    c = 3.1e1; cout<<"Assign 3.1e1 to character$ "<<c<<", ASCII-"<<int(c)<<endl;
+    c = 94.37; cout<<"Assign 94.37 to character$ "<<c<<", ASCII$ "<<int(c)<<endl;
+    c = 3.1e1; cout<<"Assign 3.1e1 to character$ "<<c<<", ASCII$ "<<int(c)<<endl;
+    // c="s"; ERROR: invalid conversion from ‘const char*’ to ‘char’ [-fpermissive]
+    
+    cout<<endl;
+    unsigned char uc='9';
+    cout<<"ASCII equivalent value for character '9'$  "<<int(uc)<<endl;
+    cout<<"Size of unsigned character "<<sizeof(uc)<<endl; // 1 byte
+    cout<<"Type of unsigned character "<<typeid(uc).name()<<endl; //h for unsigned character
+    cout<<"Range : "<<int(numeric_limits<unsigned char>::min())<<" to "<<int(numeric_limits<unsigned char>::max())<<endl;
+    
     
     return 0;
 }
