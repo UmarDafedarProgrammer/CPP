87c908dde419b9bad07346fd2153c2416bc8d838
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-12-26 06:14:03 +0530

    Create 1.5.8.FloatingPointNumbers.cpp

---
+++ 1.5.8.FloatingPointNumbers.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+Floating Point Nummber
+ - Floating point/ double data type are used to store real Numbers
+ - Float data type - 4 Bytes
+ - Double Data type - 8 bytes
+ - Float Precision - 6 decimal points
+ - Double Precision - 15 decimal points
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Floating Point Numbers"<<endl;
+    float f;
+    cout<<"f: "<<f<<endl;
+    f = 3.123456789123456789;
+    double d=3.123456789123456789;
+    long double ld=3.123456789123456789;
+    cout.precision(30);
+    cout<<"f: "<<f<<endl;
+    cout<<"d: "<<d<<endl;
+    cout<<"ld: "<<ld<<endl;
+    cout<<"size of float "<<sizeof(float)<<endl;
+    cout<<"size of double "<<sizeof(double)<<endl;
+    cout<<"size of long double "<<sizeof(long double)<<endl;
+    return 0;
+}
