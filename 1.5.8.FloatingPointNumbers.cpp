48f85a5f1e01f3b73dfbecac753b5f7c8d15ca08
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-12-26 06:15:15 +0530

    Update 1.5.8.FloatingPointNumbers.cpp

--- 1.5.8.FloatingPointNumbers.cpp
+++ 1.5.8.FloatingPointNumbers.cpp
@@ -3,8 +3,16 @@ Floating Point Nummber
  - Floating point/ double data type are used to store real Numbers
  - Float data type - 4 Bytes
  - Double Data type - 8 bytes
+ - long double data type - 16 bytes
+ 
  - Float Precision - 6 decimal points
  - Double Precision - 15 decimal points
+ - long double - 15 decimal points
+ 
+ suffixes:
+ f/F -float
+ d/D - double
+ ld/LD - long double
 *******************************************************************************/
 #include <iostream>
 
