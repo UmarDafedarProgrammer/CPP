7b051eeb2391cb5e06fef304bf8da28c529a3100
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-12-27 08:21:52 +0530

    Create 1.6.9.SignedChar.cpp

---
+++ 1.6.9.SignedChar.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+  char c=99;
+  cout<<c<<" "<<bitset<8>(c).to_string()<<" "<<int(c)<<endl;
+  
+  c=-99;
+  cout<<c<<" "<<bitset<8>(c).to_string()<<" "<<int(c)<<endl;
+  
+  c=129;
+  cout<<c<<" "<<bitset<8>(c).to_string()<<" "<<int(c)<<" "<<bitset<8>(129).to_string()<<endl;
+  
+  c=-128;
+  cout<<c<<" "<<bitset<8>(c).to_string()<<" "<<int(c)<<endl;
+  return 0;
+}
