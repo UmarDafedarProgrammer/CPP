0a95b49a516763680b30eef84c60a23839bd982c
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-01-08 18:23:09 +0530

    Create 1.9.VoidPointerArithmetic.cpp

---
+++ Pointer/1.9.VoidPointerArithmetic.cpp
@@ -0,0 +1,20 @@
+/*
+void pointer arithmetic:
+	The C standard doesn’t allow pointer arithmetic with void pointers. However, in GNU C it is allowed by considering the size of void is 1.
+	
+*/
+
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+int main()
+{
+	void* voidptr = new int(78989); // void pointer
+	cout<<"Value :"<<*(int*)voidptr<<" Address:"<<voidptr<<endl;
+	voidptr++; // Incremented by 1
+	cout<<"After incrementing the value by 1"<<endl;
+	cout<<"Value :"<<*(int*)voidptr<<" Address:"<<voidptr<<endl;
+	return 0;
+}
