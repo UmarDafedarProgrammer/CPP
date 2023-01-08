e1817e2d62266a8e320f1294e3a6cf94f2f38e99
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-01-08 18:31:42 +0530

    Create 1.10.InvalidPointers.cpp

---
+++ Pointer/1.10.InvalidPointers.cpp
@@ -0,0 +1,21 @@
+/*
+Invalid Pointers:
+	Pointers are called invalied if they are not initialized
+	In array, if the pointer is pointing to a memory address beyond the range then it is called invalid pointer
+*/
+
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+int main()
+{
+	int* ptr; // ptr is not initialized and not pointing to correct memory address. Will have junk values
+	cout<<ptr<<endl; // Having a junk value in it as it is not initialized. Invalid access
+	
+	int array1[10] = {1,2,3,4,5,6,7,8,9,10};
+	int* ptr = array1+12; // Pointer array1+12 is pointing to the memory address beyond the range. Invalid access of the memory
+	
+	return 0;
+}
