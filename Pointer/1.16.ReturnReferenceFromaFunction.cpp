24a55b5b2a35cb55f5de25519200e8a552154d9d
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-01-08 21:59:50 +0530

    Update 1.16.ReturnReferenceFromaFunction.cpp

--- Pointer/1.16.ReturnReferenceFromaFunction.cpp
+++ Pointer/1.16.ReturnReferenceFromaFunction.cpp
@@ -3,6 +3,9 @@ Return a reference from a function
 Since, a variable localVar is local to ReturnReference() fuction. Once, the execution of the ReturnReference function is completed,
 processor unwinds all the local variables created on the stack.
 So, you can not return a reference.
+
+Compiler throws a warning -
+[Warning] reference to local variable 'localVar' returned [-Wreturn-local-addr]
 */
 
 #include<iostream>
