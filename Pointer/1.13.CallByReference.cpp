ed8a88d4fa459c8727afa3ea992ff9f6c256f9d8
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-01-08 20:03:52 +0530

    Create 1.13.CallByReference.cpp

---
+++ Pointer/1.13.CallByReference.cpp
@@ -0,0 +1,42 @@
+/*
+Call By Reference
+
+*/
+
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+void callByValue(int inp)
+{
+	cout<<"callByValue: Address of inp: "<<&inp<<endl;
+	inp=33;
+	cout<<"callByValue: Value is changed to : "<<inp<<endl;
+}
+
+void callByReference(int& inp)
+{
+	cout<<"callByReference: Address of inp: "<<&inp<<endl;
+	inp=34;
+	cout<<"callByReference: value is changed to : "<<inp<<endl;
+}
+
+int main()
+{
+	cout<<"Main Func"<<endl;
+	
+	int inp=786;
+	
+	cout<<"Main: Address of inp : "<<&inp<<endl;
+	cout<<"Main: Value of inp Before call: "<<inp<<endl;
+	callByValue(inp);
+	cout<<"Main: Value of inp After callByValue call: "<<inp<<endl;
+	cout<<endl<<endl<<endl<<endl;
+	cout<<"Main: Address of inp : "<<&inp<<endl;
+	cout<<"Main: Value of inp Before call: "<<inp<<endl;
+	callByReference(inp);
+	cout<<"Main: Value of inp After callByValue call: "<<inp<<endl;
+	cout<<endl<<endl<<endl<<endl;
+	return 0;
+}
