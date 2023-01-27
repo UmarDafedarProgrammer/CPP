252278ca1a8e5bacc4590329549daa0ddbd43fcc
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-01-27 19:08:36 +0530

    Update 1.3.ScopedEnum.cpp

--- Enum/1.3.ScopedEnum.cpp
+++ Enum/1.3.ScopedEnum.cpp
@@ -4,6 +4,20 @@ Scoped enum:
 
 Scoped enum can only be accessed using the enumerator qualifier
 This is to avoid the conflicts of having two different enumerators with same name
+
+
+enum struct|class name { enumerator = constexpr , enumerator = constexpr , ... }	(1)	
+enum struct|class name : type { enumerator = constexpr , enumerator = constexpr , ... }	(2)	
+enum struct|class name ;	(3)	
+enum struct|class name : type ;	(4)	
+1) declares a scoped enumeration type whose underlying type is int (the keywords class and struct are exactly equivalent)
+2) declares a scoped enumeration type whose underlying type is type
+3) opaque enum declaration for a scoped enumeration whose underlying type is int
+4) opaque enum declaration for a scoped enumeration whose underlying type is type
+Each enumerator becomes a named constant of the enumeration's type (that is, name), 
+which is contained within the scope of the enumeration, and can be accessed using scope resolution operator. 
+There are no implicit conversions from the values of a scoped enumerator to integral types, 
+although static_cast may be used to obtain the numeric value of the enumerator.
 *******************************************************************************/
 #include <iostream>
 
