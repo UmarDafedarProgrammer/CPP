4e11ccd3749747d818715c6ac7b4e21766af1bc2
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-01-28 15:30:18 +0530

    Create 1.1UnionBasic.cpp

---
+++ Union/1.1UnionBasic.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+Union:
+	Union is a user defined data type
+	Union can hold only one of its non-static data members at a time.
+	The union is at least as big as necessary to hold its largest data member, but is usually not larger
+	The other data members are intended to be allocated in the same bytes as part of that largest member
+	It is undefined behavior to read from the member of the union that wasn't most recently written
+
+	Declaration:
+	union attr class-head-name { member-specification }
+
+	A union can have member functions (including constructors and destructors), but not virtual functions.
+	A union cannot have base classes and cannot be used as a base class.
+	A union cannot have non-static data members of reference types.
+	
+
+*/
+void Union()
+{
+	cout << "Union Data Type \n";
+	union Data
+	{
+		int i;
+		float f;
+		char c;
+		long long l;
+	};
+
+	Data d;
+	d.i = 98;
+	cout << d.i <<" "<<d.f<<" "<<d.c<<" "<<d.l<< endl;
+	d.c = 559;
+	cout << d.i << " " << d.f << " " << d.c << " " << d.l << endl;
+	cout << "Size of the Union " << sizeof(Data) << endl;
+	cout << "Size of int " << sizeof(int) << endl;
+	cout << "Size of long " << sizeof(long long) << endl;
+	return;
+}
